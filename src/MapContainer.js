6f966fe556739568108fbe00a60194323fc306df
dewangsolanki3 <dewangsolanki3@gmail.com>
2019-07-28 08:23:11 +0530

    Final Push

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
+import {Map, TileLayer, Marker, Circle, CircleMarker, Popup} from 'react-leaflet';
+import bg from './bg.jpg'
 import axios from 'axios';
 const hereCredentials = {
     id: 'z546CZ6cCDEN8pnO5p9x',
@@ -11,12 +12,14 @@ class MapContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            location1: [18.932149, 72.834192],
+            location2: [18.929048, 72.831527]
         }
     }
     
 
     render() {
+        
         const hereTileURL = `https://2.base.maps.api.here.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/512/png8?app_id=${hereCredentials.id}&app_code=${hereCredentials.code}&ppi=320`;
         console.log(this.props.center);
         if(!this.props.center[0] || !this.props.center[1]) {
@@ -24,7 +27,8 @@ class MapContainer extends Component {
         }
         console.log('this is the coord', this.props.center);
         return (
-            
+            <div>
+            <div className= "mobile-container">
             <Map 
         center = {this.props.center}
         zoom = {this.props.zoom}
@@ -33,12 +37,20 @@ class MapContainer extends Component {
           attribution='&copy; Here 2019'
           url={hereTileURL}
         />
+        <CircleMarker center={this.props.center} color="red" radius={10}></CircleMarker>
+        {/* <CircleMarker center={this.state.location1} color="black" radius={10}></CircleMarker>
+        <CircleMarker center={this.state.location2} color="black" radius={10}></CircleMarker> */}
+        <Circle center={this.props.center} fillColor="blue" radius={400} />
+        <Circle center={this.state.location1} fillColor="blue" radius={30} />
+        <Circle center={this.state.location2} fillColor="blue" radius={30} />
+        <Circle center={this.props.center} fillColor="blue" radius={1000} />
         
         <Marker position = {this.props.center} />
-        <Marker position = {[18.932149, 72.834192]} />
-        <Marker position = {[18.929048, 72.831527]} />
+        {/* <Marker position = {this.state.location1} />
+        <Marker position =  {this.state.location2}/> */}
         </Map>
-            
+        </div>
+        </div>
         )
     }
 
